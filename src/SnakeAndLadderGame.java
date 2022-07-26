105a9485a44e338fc06cf57fb9d3c51afdd0df88
Prabhas3699 <prabhak620@gmail.com>
2022-07-26 18:39:20 +0530

    Snake and Ladder game start

---
+++ src/SnakeAndLadderGame.java
@@ -0,0 +1,14 @@
+
+public class SnakeAndLadderGame {
+
+	public static void main(String[] args) {
+		//variables
+		int position=0;
+		
+		//printing the output
+		System.out.println("Snake and Ladder Game with Single player");
+		System.out.println("The Game Start at position "+position);
+
+	}
+
+}
